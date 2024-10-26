7b0697471509e8d58649a7c7cad49611d6062c79
Maneren <maneren731@gmail.com>
2024-10-26 14:15:17 +0200

    fix bounds checking

--- src/instructions/common.c
+++ src/instructions/common.c
@@ -58,24 +58,24 @@ int vm_get_reg(VM *vm, Byte reg, Number *out) {
 }
 
 int vm_code_read_reg(VM *vm, size_t address, Byte *out) {
-  ASSERT(address + sizeof(Byte) < vm->code_size);
+  ASSERT(address + sizeof(Byte) <= vm->code_size);
   *out = vm->code_segment[address];
   return EXIT_SUCCESS;
 }
 
 int vm_code_read_im32(VM *vm, size_t address, Number *out) {
-  ASSERT(address + sizeof(Number) < vm->code_size);
+  ASSERT(address + sizeof(Number) <= vm->code_size);
   *(out) = *(Number *)(vm->code_segment + address);
   return EXIT_SUCCESS;
 }
 
 int vm_read_im32(VM *vm, Number address, Number *out) {
-  ASSERT(address >= 0 && (size_t)address < vm->data_size);
+  ASSERT(address >= 0 && (size_t)address + sizeof(Number) <= vm->data_size);
   *out = *(Number *)(vm->data_segment + address);
   return EXIT_SUCCESS;
 }
 int vm_write_im32(VM *vm, Number address, Number im32) {
-  ASSERT(address >= 0 && (size_t)address < vm->data_size);
+  ASSERT(address >= 0 && (size_t)address + sizeof(Number) <= vm->data_size);
   *(Number *)(vm->data_segment + address) = im32;
   return EXIT_SUCCESS;
 }
