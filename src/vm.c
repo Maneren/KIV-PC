1412831332d4a136dccddcebc3a52caecd1c77c8
Maneren <maneren731@gmail.com>
2024-10-30 15:05:09 +0100

    replace switch with explicit table

--- src/vm.c
+++ src/vm.c
@@ -109,76 +109,20 @@ int vm_run(VM *vm) {
     vm->IP++;
     vm->instructions_count++;
 
-    switch (instruction) {
-    case 0x00: // HALT
+    if (instruction == 0x00) { // HALT
       halted = 1;
       break;
-    case 0x90: // NOP
-      break;
+    }
 
-      // DATA INSTRUCTIONS
-      INSTRUCTION(0x10, vm_mov_reg_im32);  // MOV reg, im32
-      INSTRUCTION(0x11, vm_mov_reg_reg);   // MOV regd, regs
-      INSTRUCTION(0x12, vm_movsd);         // MOVSD
-      INSTRUCTION(0x13, vm_load_reg_im32); // LOAD reg, im32
-      INSTRUCTION(0x14, vm_load_reg_reg);  // LOAD regd, regs
-      INSTRUCTION(0x15, vm_stor_reg_im32); // STOR reg, im32
-      INSTRUCTION(0x16, vm_stor_reg_reg);  // STOR regd, regs
+    instruction_handler_t handler = instruction_table[instruction];
 
-      // STACK INSTRUCTIONS
-      INSTRUCTION(0x20, vm_push); // PUSH reg
-      INSTRUCTION(0x21, vm_pop);  // POP reg
-
-      // ARITHMETIC INSTRUCTIONS
-      INSTRUCTION(0x30, vm_add_reg_im32); // ADD reg, im32
-      INSTRUCTION(0x31, vm_add_reg_reg);  // ADD regd, regs
-      INSTRUCTION(0x32, vm_sub_reg_im32); // SUB reg, im32
-      INSTRUCTION(0x33, vm_sub_reg_reg);  // SUB regd, regs
-      INSTRUCTION(0x34, vm_mul_reg_im32); // MUL reg, im32
-      INSTRUCTION(0x35, vm_mul_reg_reg);  // MUL regd, regs
-      INSTRUCTION(0x36, vm_div_reg_im32); // DIV reg, im32
-      INSTRUCTION(0x37, vm_div_reg_reg);  // DIV regd, regs
-      INSTRUCTION(0x38, vm_inc_reg);      // INC reg
-      INSTRUCTION(0x39, vm_dec_reg);      // DEC reg
-
-      // BITSHIFT INSTRUCTIONS
-      INSTRUCTION(0x50, vm_shl_reg_im32); // SHL reg, im32
-      INSTRUCTION(0x51, vm_shl_reg_reg);  // SHL regd, regs
-      INSTRUCTION(0x52, vm_shr_reg_im32); // SHR reg, im32
-      INSTRUCTION(0x53, vm_shr_reg_reg);  // SHR regd, regs
-
-      // COMPARISON INSTRUCTIONS
-      INSTRUCTION(0x60, vm_cmp_reg_im32); // CMP reg, im32
-      INSTRUCTION(0x61, vm_cmp_reg_reg);  // CMP regd, regs
-
-      // JUMP INSTRUCTIONS
-      INSTRUCTION(0x70, vm_jmp_im32); // JMP im32
-      INSTRUCTION(0x71, vm_jmp_reg);  // JMP reg
-      INSTRUCTION(0x72, vm_je_im32);  // JE im32
-      INSTRUCTION(0x73, vm_jne_im32); // JNE im32
-      INSTRUCTION(0x74, vm_jg_im32);  // JG im32
-      INSTRUCTION(0x75, vm_jge_im32); // JGE im32
-      INSTRUCTION(0x76, vm_jng_im32); // JNG im32
-      INSTRUCTION(0x77, vm_jl_im32);  // JL im32
-      INSTRUCTION(0x78, vm_jle_im32); // JLE im32
-      INSTRUCTION(0x79, vm_jnl_im32); // JNL im32
-
-      // CALL INSTRUCTIONS
-      INSTRUCTION(0x80, vm_call_im32); // CALL im32
-      INSTRUCTION(0x81, vm_call_reg);  // CALL reg
-      INSTRUCTION(0x82, vm_ret);       // RET
-
-      // IO INSTRUCTIONS
-      INSTRUCTION(0xF0, vm_outd_reg); // OUTD reg
-      INSTRUCTION(0xF1, vm_outc_reg); // OUTC reg
-      INSTRUCTION(0xF2, vm_outs_reg); // OUTS reg
-      INSTRUCTION(0xF3, vm_inpd_reg); // INPD reg
-      INSTRUCTION(0xF4, vm_inpc_reg); // INPC reg
-      INSTRUCTION(0xF5, vm_inps_reg); // INPS reg
-    default:
-      fprintf(stderr, "Unknown instruction: 0x%02x\n", instruction);
+    if (!handler) {
+      fprintf(stderr, "Unknown instruction at address 0x%08lX\n", vm->IP);
       return EXIT_FAILURE;
     }
+
+    if (handler(vm))
+      return EXIT_FAILURE;
 
     if (instruction != 0x60 && instruction != 0x61)
       vm->flags = 0;
